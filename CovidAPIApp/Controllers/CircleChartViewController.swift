15cbd1c408f8bb37197de3c7a7c37425e1465fe7
haru125 <jhonathan1471@gmail.com>
2021-08-16 14:13:05 +0900

    1234

---
+++ CovidAPIApp/Controllers/CircleChartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CircleChartViewController.swift
+//  CovidAPIApp
+//
+//  Created by Satoshi Ota on 2021/08/16.
+//
+
+import UIKit
+
+class CircleChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CovidAPIApp/Models/CovidSingleton.swift
@@ -0,0 +1,8 @@
+//
+//  CovidSingleton.swift
+//  CovidAPIApp
+//
+//  Created by Satoshi Ota on 2021/08/16.
+//
+
+import Foundation
